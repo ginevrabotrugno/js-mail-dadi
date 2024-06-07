70b95309da7c67f5b3fd76fdf2c143e88c69b5b4
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-07 18:09:48 +0200

    style: fix code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// EMAIL PROGRAM
+
 // 1.Chiedi all’utente la sua email, 
 // 1a.inserisci l'input che sarà la costante email
 const userEmail= document.getElementById("user_input").value
@@ -29,14 +31,9 @@ buttonAccedi.addEventListener("click",
 )
 
 
-    
-
-    
-
-
+// DADI PROGRAM
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
-
 const buttonLancia = document.getElementById("lancia");
 
 buttonLancia.addEventListener ("click",
@@ -60,14 +57,3 @@ buttonLancia.addEventListener ("click",
 
     }
 )
-
-
-
-
-
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
